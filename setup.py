df0b2f1667fd92e6b087c0065ce1fe8541d65ecf
Ben Jeffrey <mail@benjeffrey.com>
2014-03-05 04:58:15 +0000

    Increment version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='daterange',
-    version='0.2',
+    version='0.3',
     description='Like xrange(), but for datetime objects.',
     author='Zachary Voase',
     author_email='zacharyvoase@me.com',
